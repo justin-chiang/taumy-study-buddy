63abbcd9b6303a25cb179d7fd3b60b0f1d2fb79c
Bryan Lee <78395163+bryan901@users.noreply.github.com>
2022-11-15 15:23:55 -0800

    Login page basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import Welcome from './pages/welcome';
+import './styles/welcome.css'
+import {BrowserRouter as Router, Switch, Route, useNavigate} from 'react-router-dom';
+import Login from "./pages/login";
 
 function App() {
+  
   return (
-    <div className="App">
-      <Welcome/>
-    </div>
+    // <Router>
+    //   <div className = "main">
+    //       <h1 className = "header"> Welcome to Taumy! </h1>
+    //       <div className = "buttons">
+    //           <button className="login" type="submit"> Login </button>
+    //           <button className="signup" type="submit"> Signup </button>
+    //       </div>
+    //   </div>
+    // </Router>
+    <Login/>
   );
 }
 
